6be49500dff8a036f95ddf328fc7f2cd8bb51f05
kai studio <97531705+kaiproduction@users.noreply.github.com>
2024-11-15 10:31:20 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA4BVG7Ypwa6VI0ADTEcqwHj6px_CGCUoY",
-  authDomain: "socialnetwork-37297.firebaseapp.com",
-  databaseURL: "https://socialnetwork-37297-default-rtdb.firebaseio.com",
-  projectId: "socialnetwork-37297",
-  storageBucket: "socialnetwork-37297.appspot.com",
-  messagingSenderId: "775756757662",
-  appId: "1:775756757662:web:6f08910940368b8cb8198f",
-  measurementId: "G-K163T0HH32"
+  apiKey: "", // Your API key
+  authDomain: "", // Your authDomain
+  databaseURL: "", // Your databaseURL
+  projectId: "", // Your projectId
+  storageBucket: "", // Your storageBucket
+  messagingSenderId: "", // Your messagingSenderId
+  appId: "", // Your appId
+  measurementId: "" // measurementId
 };
 
 // Initialize Firebase
